7e2b5ef56654975aea9f8bfe18351409e9661630
fatimakhosravi <fateme.khosravi3307@gmail.com>
2024-09-03 13:35:15 +0330

    develop: removed some svg files ,refactor some files and add navbar component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/components/Home/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <HeroSection />
     </main>
   );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { NavbarData } from "./NavbarData";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/../public/jpg/bread-farm-logo.jpg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="pt-6">
+      <div className="flex flex-row justify-center gap-4 grad bg-gradient-to-r from-[#ebdbbe] p-4 rounded-xl w-full shadow-md shadow-[#70695c]">
+        <Image className="w-24 rounded-md" alt="logo" src={Logo} />
+        {NavbarData.map((item, index) => (
+          <div
+            className="flex flex-row items-center justify-center"
+            key={index}
+          >
+            <Link className="text-xl font-bold hover:text-[#f5bb4f]" href={item.href}>
+              {item.link_name}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
